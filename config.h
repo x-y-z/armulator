a10a3d935cc78dbfe7acb8c7724cc5856e483b0d
Zi Yan <birdmanyan@gmail.com>
2011-11-19 22:18:59 -0500

    compilable code

---
+++ config.h
@@ -0,0 +1,20 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "birdmanyan@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "armulator"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "armulator 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "ARMulator"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://github.com/x-y-z/armulator"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
